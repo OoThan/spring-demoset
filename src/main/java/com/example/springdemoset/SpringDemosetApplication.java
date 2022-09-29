cf1e73719ce860881567222b70b885121cb5802b
OoThan <mgoothan.psnl@gmail.com>
2022-09-28 20:44:03 -0700

    fitst init

---
+++ src/main/java/com/example/springdemoset/SpringDemosetApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdemoset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemosetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemosetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdemoset/SpringDemosetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemoset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemosetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
